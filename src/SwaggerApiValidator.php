af7f8ea9861b97afaa0fb20c6952c2ec86180645
Ваше Имя <you@example.com>
2019-10-25 17:55:03 +0700

    some work

--- src/SwaggerApiValidator.php
+++ src/SwaggerApiValidator.php
@@ -2,11 +2,25 @@
 
 namespace Codeception\Module;
 
+use Codeception\Lib\InnerBrowser;
 use Codeception\Lib\Interfaces\DependsOnModule;
 use Codeception\Module;
+use Garden\Schema\RefNotFoundException;
+use Garden\Schema\Schema;
+use Symfony\Component\BrowserKit\Request;
+use Symfony\Component\BrowserKit\Response;
 
 class SwaggerApiValidator extends Module implements DependsOnModule
 {
+    /**
+     * Конфигурация модуля.
+     *
+     * @var array
+     */
+    protected $config = [
+        'swagger' => ''
+    ];
+
     /**
      * Сообщение при неверной конфигурации.
      *
@@ -23,6 +37,16 @@ modules:
 EOF;
 
     /**
+     * @var InnerBrowser
+     */
+    protected $innerBrowser;
+
+    /**
+     * @var REST
+     */
+    protected $rest;
+
+    /**
      * Specifies class or module which is required for current one.
      *
      * THis method should return array with key as class name and value as error message
@@ -33,5 +57,59 @@ EOF;
     public function _depends(): array
     {
         return [REST::class => $this->dependencyMessage];
+    }
+
+    /**
+     * Устанавливаем ссылки на модули, с которыми будем работать.
+     *
+     * @param REST         $rest
+     * @param InnerBrowser $innerBrowser
+     */
+    public function _inject(REST $rest, InnerBrowser $innerBrowser)
+    {
+        $this->rest = $rest;
+        $this->innerBrowser = $innerBrowser;
+    }
+
+    /**
+     * Получаем отправленный запрос в формате BrowserKit.
+     *
+     * @return Request
+     */
+    public function getRequest(): Request
+    {
+        return $this->rest->client->getInternalRequest();
+    }
+
+    /**
+     * Получаем ответ на запрос в формате BrowserKit.
+     *
+     * @return Response
+     */
+    public function getResponse(): Response
+    {
+        return $this->rest->client->getInternalResponse();
+    }
+
+    /**
+     * Получаем объект схемы, описанной в swagger-файле.
+     *
+     * @return Schema
+     */
+    public function getSchema()
+    {
+        return Schema::parse(yaml_parse_file($this->config['swagger']));
+    }
+
+    public function seeRequestIsValid()
+    {
+        $schema = $this->getSchema();
+        $request = $this->getRequest();
+        try {
+            $result = $schema->isValid($request, ['request' => true]);
+        } catch (RefNotFoundException $e) {
+            $result = false;
+        }
+        return $result;
     }
 }
